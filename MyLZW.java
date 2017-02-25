fafa26f317990ff0b2cce1e72448c2cde47e75b4
jaranimo95 <cjarani1608@hotmail.com>
2017-02-25 15:22:30 -0500

    Debugging Monitor Mode

--- MyLZW.java
+++ MyLZW.java
@@ -16,36 +16,95 @@
  *
  *************************************************************************/
 
-public class MyLZW {
-    private static final int R = 256;        // number of input chars
-    private static final int L = 4096;       // number of codewords = 2^W
-    private static final int W = 12;         // codeword width
+// Replace substring with a method using 2 indices & a char array (one indice represents beginning of new substring, the other for the end, with char array containing the data between them)
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.File;
 
-    public static void compress() { 
-        String input = BinaryStdIn.readString();
-        TST<Integer> st = new TST<Integer>();
+public class MyLZW {
+	private static final double comprThreshold = 1.1;
+    private static final int R = 256;        // number of input chars
+    private static int L = 512;       		 // number of codewords = 2^W
+    private static int W = 9;         		 // codeword width
+
+    public static void compress(char mode) throws FileNotFoundException{
+
+    	PrintWriter pw = new PrintWriter(new File("output.txt"));
+
+        String input = BinaryStdIn.readString();	  // reads in filename to compress
+        TST<Integer> st = new TST<Integer>();			 
         for (int i = 0; i < R; i++)
-            st.put("" + (char) i, i);
+            st.put("" + (char) i, i);				  // Populates st with keys all 8-bit extended ASCII values (0-255)
         int code = R+1;  // R is codeword for EOF
 
+        String prefix = "";
+        int t = 0;
+
+        boolean monitorFlag = false;
+
+        long consumed = 0;		// Data that's been read in from the file
+        long compressed = 0;	// Data that's been compressed
+        double ratioOld = 1;	// Old compression ratio
+        double ratioCurr = 0;	// Current compression ratio
+
         while (input.length() > 0) {
-            String s = st.longestPrefixOf(input);  // Find max prefix match s.
-            BinaryStdOut.write(st.get(s), W);      // Print s's encoding.
-            int t = s.length();
-            if (t < input.length() && code < L)    // Add s to symbol table.
-                st.put(input.substring(0, t + 1), code++);
-                // If write fails bc st (codebook) is full, increment W by 1
-                // Might have to go back thru codebook and reset values to new width W
+
+			if (code == L && W == 16 && !monitorFlag) {        	   
+         	   // Reset codebook with initial contents (0->255 (0->R-1) used)	
+	         	if (mode == 'r'){	
+	         		st = new TST<Integer>();
+	        		for (int i = 0; i < R; i++)
+	            		st.put("" + (char) i, i);
+	         		W = 9;												// Set codeword width back to initial state of 9 bits
+	         		L = 512;											// Set max number of codewords to 2^W = 2^9 = 512
+	         		code = R+1;											// Set code back to R+1
+         		}
+         	   // Save current compression ratio to begin monitoring ratio of compression ratios	
+         		else if (mode == 'm') {
+         			ratioOld = ratioCurr;
+         			monitorFlag = true;		// Monitor Mode: ON
+         		}
+         	}
+
+         	prefix = st.longestPrefixOf(input);					// Find max prefix match in input
+            t = prefix.length();
+            compressed += W;
+            consumed += ( t*16 );
+            ratioCurr = ( (double) consumed / (double) compressed );
+
+            System.err.println("Ratio = " + ratioCurr);
+
+            if (t < input.length() && code < L)					// If prefix is shorter than input and still space for more codewords
+                st.put(input.substring(0, t + 1), code++);			// Add prefix to symbol table.
+         	else if (code == L && W < 16) {						// Else if symbol table is full (max # of codewords) and width is less than 16 (RESIZE)
+         		L = (int) Math.pow(2,++W);							// Increase codeword width and max amount of codewords         	
+         		code++;
+         	}
+
+         	if ( ( ratioOld / ratioCurr ) >= comprThreshold && monitorFlag) {
+            	st = new TST<Integer>();
+	        		for (int i = 0; i < R; i++)
+	            		st.put("" + (char) i, i);
+	         		W = 9;												// Set codeword width back to initial state of 9 bits
+	         		L = 512;											// Set max number of codewords to 2^W = 2^9 = 512
+	         		code = R+1;											// Set code back to R+1
+	         		monitorFlag = false;
+            }
+
+         	//pw.write(st.get(prefix)+", "+prefix+", "+code);
+         	//pw.println();
             input = input.substring(t);            // Scan past s in input.
+        
         }
-        BinaryStdOut.write(R, W);
+        pw.close();
+        BinaryStdOut.write(R,W);	
         BinaryStdOut.close();
     } 
 
 
     public static void expand() {
         String[] st = new String[L];			// Should be large enough to handle max # of codewords
-        int i; // next available codeword value
+        int i; 									// next available codeword value
 
         // initialize symbol table with all 1-character strings
         for (i = 0; i < R; i++)
@@ -68,10 +127,15 @@ public class MyLZW {
         BinaryStdOut.close();
     }
 
-
-
-    public static void main(String[] args) {
-        if      (args[0].equals("-")) compress();
+    public static void main(String[] args) throws FileNotFoundException {
+        	 if (args[0].equals("-")) {
+        	 	char mode = args[1].charAt(0);
+        	 	String validOptions = "nmr";
+        	 	if(validOptions.indexOf(mode) < 0)
+        	 		System.out.println("\nInvalid Mode - Valid choices are as follows:\n\tDo Nothing: 'n'\n\t   Monitor: 'm'\n\t     Reset: 'r'");
+        	 	else 
+        	 		compress(mode);
+        	 }
         else if (args[0].equals("+")) expand();
         else throw new IllegalArgumentException("Illegal command line argument");
     }
